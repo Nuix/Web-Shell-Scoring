455a62d54030d3b4a621c27820785a1de2acd17d
Michael Kamida <mkamida01@nuix.com>
2019-03-26 07:22:52 -1000

    reviewed

--- Project/WSS_WebShell_Scoring.rb
+++ Project/WSS_WebShell_Scoring.rb
@@ -1,53 +1,51 @@
+# Scores text based on rules.
+# Adds custom metadata "WebShell Score"
 def nuix_worker_item_callback(worker_item)
+  rules = {
+    'Copyright \\(c\\) 1997-2010 The PHP Group' => -200,
+    'eval\\(' => 25,
+    'base64_decode\\(' => 25,
+    'gzinflate' => 25,
+    'passthru\\(' => 25,
+    'proc_open\\(' => 25,
+    'system\\(' => 25,
+    'shell_exec\\(' => 25,
+    'cfexecute' => 25,
+    'cmd.exe' => 25,
+    'runcommand' => 25,
+    'mdEncode' => 25,
+    'zaco' => 25,
+    'zippo' => 25,
+    'XiX_' => 25,
+    'Nuke Shell' => 25,
+    'Reverse Shell' => 25,
+    'cleanCC\\(' => 25,
+    'r57sh' => 25,
+    'c99sh' => 25,
+    'uZE Shell' => 25,
+    'TC9A16C47DA8EEE87' => 50,
+    'webshell' => 25,
+    'Nickserv.*identify' => 50,
+    'laudanum' => 50,
+    'web shell' => 25,
+    '0rb' => 25,
+    'orb' => 25,
+    'SQL Dumper' => 100,
+    '\\\\x70\\\\x72\\\\x65\\\\x67\\\\x5f\\\\x72\\\\x65\\\\x70\\\\x6c\\\\x61\\\\x63\\\\x65' => 100,
+    '\\\\x65\\\\x76\\\\x61\\\\x6C' => 100,
+    '\\\\x28\\\\x62\\\\x61\\\\x73\\\\x65\\\\x36' => 100
+  }
 
-    rules = {
-        "Copyright \\(c\\) 1997-2010 The PHP Group" => -200 ,
-        "eval\\(" => 25,
-        "base64_decode\\(" => 25,
-        "gzinflate" => 25,
-        "passthru\\(" => 25, 
-        "proc_open\\(" => 25,
-        "system\\(" => 25,
-        "shell_exec\\(" => 25,
-        "cfexecute" => 25,
-        "cmd.exe" => 25,
-        "runcommand" => 25,
-        "mdEncode" => 25,
-        "zaco" => 25,
-        "zippo" => 25,
-        "XiX_" => 25,
-        "Nuke Shell" => 25,
-        "Reverse Shell" => 25,
-        "cleanCC\\(" =>25,
-        "r57sh" => 25,
-        "c99sh" => 25,
-        "uZE Shell" => 25,
-        "TC9A16C47DA8EEE87" => 50,
-        "webshell" => 25,
-        "Nickserv.*identify" => 50,
-        "laudanum" => 50,
-        "web shell" => 25,
-        "0rb" => 25,
-        "orb" => 25,
-        "SQL Dumper" => 100, 
-        '\\\\x70\\\\x72\\\\x65\\\\x67\\\\x5f\\\\x72\\\\x65\\\\x70\\\\x6c\\\\x61\\\\x63\\\\x65' => 100,
-        '\\\\x65\\\\x76\\\\x61\\\\x6C' => 100,
-        '\\\\x28\\\\x62\\\\x61\\\\x73\\\\x65\\\\x36' => 100,
-    }
-
-    source_item = worker_item.source_item
-    begin
-        if(source_item.getType().to_s =~ /text/ or source_item.getType().to_s =~ /script/)
-            score = 0
-            data =  source_item.text.to_s
-
-            rules.keys.each do |k|
-                score += rules[k] if data =~ /#{k}/i
-            end
-
-            worker_item.add_custom_metadata("WebShell Score", score, "integer", "user")
-        end
-    rescue
-        return
+  source_item = worker_item.source_item
+  type = source_item.get_type.get_name
+  begin
+    if type =~ /text/ || type =~ /script/
+      data = source_item.get_text.to_string
+      score = 0
+      rules.each { |k, v| score += v if data =~ /#{k}/i }
+      worker_item.add_custom_metadata('WebShell Score', score, 'integer', 'user')
     end
+  rescue StandardError
+    return
+  end
 end
